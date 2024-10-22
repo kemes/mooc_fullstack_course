05f08af2abbe39743098860b470b4b33a0fe7329
Mikael Hirn <mikael.hirn@gmail.com>
2024-10-22 13:35:35 +0300

    wip: 2.18

--- part2/dataForCountries/src/App.jsx
+++ part2/dataForCountries/src/App.jsx
@@ -1,17 +1,46 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import axios from 'axios'
 
 function App() {
 	const [searchText, setSearchText] = useState('')
-	const [result, setResult] = useState('')
+	const [result, setResult] = useState([])
+	let url = "https://studies.cs.helsinki.fi/restcountries/api/all"
+
+	useEffect(() => {
+		axios
+		.get(url)
+		.then(response => {
+			const countries = response.data.filter((x) => x.name.common.toLowerCase().includes(searchText)).map((x) => x.name.common)
+			if(countries.length > 10 && searchText.length !== 0){
+				setResult(['Too many countries, specify another filter.'])
+			}else if(countries.length === 1){
+				axios
+				.get(`https://studies.cs.helsinki.fi/restcountries/api/name/${countries[0]}`)
+				.then(response => {
+					setResult([response.data.name.common])
+				})
+
+			}else if(searchText.length !== 0){
+				setResult(countries.map((x) => <p key={x}>{x}</p>))
+			}else{
+			setResult([])
+			}
+
+		})
+		.catch(error => {
+			console.log(`Fetch error ${error}`)
+		})
+
+	},[searchText])
 
 	const handleSearch = (event) => {
-		console.log(event.target.value)
+		setSearchText(event.target.value)
 	}
 
 
 	return (
 		<div>
-			find countries:<input onChange={handleSearch}/>
+			find countries:<input onChange={handleSearch}/><br/>
 			{result}
 		</div>
 	)
